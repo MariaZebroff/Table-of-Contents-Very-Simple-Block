2377571e85033188904d61e4f40d2c6c4b877125
Maria Zebroff <zebroffmaria12@gmail.com>
2025-02-11 19:59:27 +0500

    adding color to the header

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e44a9a4fcc1766c4bbb6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b366ccc12fa96d16a184');
--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-i18n'), 'version' => 'aaad2a30e557597e0770');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-i18n'), 'version' => '8ddc994459eb537f3989');
